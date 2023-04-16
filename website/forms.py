4493cb12d7d83c9db1f2986e04d3c17d3fb0871e
Tabriz Naghiyev <tabriznagiyev@yahoo.com>
2023-04-16 16:52:48 +0200

    Final

--- website/forms.py
+++ website/forms.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
 from django.contrib.auth.models import User
+from django import forms
+from .models import Record
 
 class SignUpForm(UserCreationForm):
-	email = forms.EmailField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'Email Address'}))
-	first_name=forms.CharField(label="",max_length=100, widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'First Name'}))
-	last_name= forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'Last Name'}))
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+	first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+	last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
 
 
 	class Meta:
 		model = User
-		fields=('username','first_name','last_name','email','password1','password2')
-		
+		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+
 	def __init__(self, *args, **kwargs):
 		super(SignUpForm, self).__init__(*args, **kwargs)
 
